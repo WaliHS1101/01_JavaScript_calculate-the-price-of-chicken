137bd09a7b69399464be8f125bf5dcc685ebf651
WHS <walih1101@gmail.com>
2023-04-01 03:24:47 +0500

    Assignment 01 Javascript

---
+++ index01.js
@@ -0,0 +1,6 @@
+let gram = prompt("Price of the Chicken per Kg is 500 , How much gram you like to order ");
+
+let Bill = 500 / 1000 * gram ;
+
+
+document.write( "You have to pay ", Bill, " Rs for ",gram," grams.");
